a96f76c5c9d23ad6190b95a6e4e1b40a69a8da92
Frans Englich <fenglich@fastmail.fm>
2021-09-04 12:59:25 +0200

    Initial add.

---
+++ download_tickers.sh
@@ -0,0 +1,3 @@
+# https://stackoverflow.com/questions/4572153/os-x-equivalent-of-linuxs-wget
+# "I'm going to have to say curl http://127.0.0.1:8000 -o outfile"
+
---
+++ frontier.m
@@ -0,0 +1,11 @@
+
+
+%%
+% The data.
+
+% The standard deviations
+sds = [23, 45, 37];
+
+% Returns
+rs = [0.02, 0.05, 0.04];
+
